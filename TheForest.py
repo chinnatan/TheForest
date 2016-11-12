02ceb01802d95740ec1bb5162c18aa6ae5965882
chinnatan <59070040@it.kmitl.ac.th>
2016-11-12 22:02:30 +0700

    Graph 1 finished thread

--- TheForest.py
+++ TheForest.py
@@ -5,10 +5,11 @@ import matplotlib.pyplot as plt
 def theforest():
     """this function will open and read file, return data."""
     yeardata = []
-    north = []
-    northeast = []
-    float_north = []
-    float_northeast = []
+    north, float_north = [], []
+    northeast, float_northeast = [], []
+    east, float_east = [], []
+    central, float_central = [], []
+    south, float_south = [], []
 
     ifile  = open('DatabaseCSV/พื้นที่ป่าประเทศไทยปี 2516 - 2558.csv', 'r')
     reader = csv.reader(ifile)
@@ -17,6 +18,9 @@ def theforest():
         yeardata.append(row[0])
         north.append(row[1])
         northeast.append(row[2])
+        east.append(row[3])
+        central.append(row[4])
+        south.append(row[5])
 
     for i in north[1:20]:
         i = i.replace(",", "")
@@ -26,7 +30,19 @@ def theforest():
         i = i.replace(",", "")
         float_northeast.append(float(i))
 
-    return yeardata[1:20], float_north, float_northeast, north, northeast
+    for i in east[1:20]:
+        i = i.replace(",", "")
+        float_east.append(float(i))
+
+    for i in central[1:20]:
+        i = i.replace(",", "")
+        float_central.append(float(i))
+
+    for i in south[1:20]:
+        i = i.replace(",", "")
+        float_south.append(float(i))
+
+    return yeardata[1:20], float_north, float_northeast, float_east, float_central, float_south
 
 plotgraph = theforest() # Call Function
 
@@ -36,16 +52,19 @@ width = 0.10       # the width of the bars
 
 fig, plot = plt.subplots()
 rects1 = plot.bar(ind, plotgraph[1], width, color='b')
-rects2 = plot.bar(ind + width, plotgraph[2], width, color='r')
+rects2 = plot.bar(ind + width, plotgraph[2], width, color='orangered')
+rects3 = plot.bar(ind + (width * 2), plotgraph[3], width, color='m')
+rects4 = plot.bar(ind + (width * 3), plotgraph[4], width, color='y')
+rects5 = plot.bar(ind + (width * 4), plotgraph[5], width, color='navy')
 
 # add some text for labels, title and axes ticks
 plot.set_ylabel('Analyzed')
 plot.set_title('Forestry analyzed by region')
-plot.set_xticks(ind + width)
+plot.set_xticks(ind + (width * 2.5))
 plot.set_xticklabels(plotgraph[0])
 plot.set_yticklabels(('0.00', '10,000,000.00', '20,000,000.00', '30,000,000.00', '40,000,000.00', '50,000,000.00', '60,000,000.00', '70,000,000.00', '80,000,000.00'))
 
-plot.legend((rects1[0], rects2[0]), ('North', 'North East'))
+plot.legend((rects1[0], rects2[0], rects3[0], rects4[0], rects5[0]), ('North', 'North East', 'East', 'Central', 'South'))
 
 def autolabel(rects):
     # attach some text labels
@@ -57,5 +76,8 @@ def autolabel(rects):
 
 autolabel(rects1)
 autolabel(rects2)
+autolabel(rects3)
+autolabel(rects4)
+autolabel(rects5)
 
 plt.show()
