3f9048b9670b8b17249b58cfbf70c68db22d33f1
chinnatan <59070040@it.kmitl.ac.th>
2016-11-12 15:05:35 +0700

    Add module matplotlib and plotgraph from databases
    Add module matplotlib, plotgraph from databases and show graph

--- TheForest.py
+++ TheForest.py
@@ -1,21 +1,61 @@
 import csv
+import numpy as np
+import matplotlib.pyplot as plt
 
 def theforest():
+    """this function will open and read file, return data."""
+    yeardata = []
+    north = []
+    northeast = []
+    float_north = []
+    float_northeast = []
+
     ifile  = open('DatabaseCSV/พื้นที่ป่าประเทศไทยปี 2516 - 2558.csv', 'r')
-    reader2 = csv.reader(ifile)
+    reader = csv.reader(ifile)
 
-    rownum = 0
-    for row in reader2:
-        # Save header row.
-        if rownum == 0:
-            header = row
-        else:
-            colnum = 0
-            for col in row:
-                print(("%s: %s") % (header[colnum], col))
-                colnum += 1
-        rownum += 1
+    for row in reader:
+        yeardata.append(row[0])
+        north.append(row[1])
+        northeast.append(row[2])
 
-    ifile.close()
+    for i in north[1:20]:
+        i = i.replace(",", "")
+        float_north.append(float(i))
 
-theforest()
+    for i in northeast[1:20]:
+        i = i.replace(",", "")
+        float_northeast.append(float(i))
+
+    return yeardata[1:20], float_north, float_northeast, north, northeast
+
+plotgraph = theforest() # Call Function
+
+num_graph = 19
+ind = np.arange(num_graph)  # the x locations for the groups
+width = 0.10       # the width of the bars
+
+fig, plot = plt.subplots()
+rects1 = plot.bar(ind, plotgraph[1], width, color='b')
+rects2 = plot.bar(ind + width, plotgraph[2], width, color='r')
+
+# add some text for labels, title and axes ticks
+plot.set_ylabel('Analyzed')
+plot.set_title('Forestry analyzed by region')
+plot.set_xticks(ind + width)
+plot.set_xticklabels(plotgraph[0])
+plot.set_yticklabels(('0.00', '10,000,000.00', '20,000,000.00', '30,000,000.00', '40,000,000.00', '50,000,000.00', '60,000,000.00', '70,000,000.00', '80,000,000.00'))
+
+plot.legend((rects1[0], rects2[0]), ('North', 'North East'))
+
+def autolabel(rects):
+    # attach some text labels
+    for rect in rects:
+        height = rect.get_height()
+        # plot.text(rect.get_x() + rect.get_width()/2., 2.05*height,
+                # '%d' % int(height),
+                # ha='center', va='bottom')
+
+autolabel(rects1)
+autolabel(rects2)
+
+plt.show()
